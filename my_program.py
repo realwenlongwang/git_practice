e395bc3a59d09f2ba0f7e7b367cbe447fee65c7c
Wenlong(Stan) Wang <stan.wenlong.wang@gmail.com>
2022-01-12 12:15:43 +1300

    Include a second crazy function to test.

--- my_program.py
+++ my_program.py
@@ -4,6 +4,10 @@ def my_function():
 def my_crazy_function_test():
 	print('I have tested git restore. This is a crazy function test I want to commit.')
 
+def my_second_crazy_function_test():
+	print('This a second crazy function!')
+
 if __name__ == "__main__":
 	my_function()
 	my_crazy_function_test()
+	my_second_crazy_function_test()
