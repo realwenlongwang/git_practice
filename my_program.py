be06a33e33618cee8d56269b642f37f94db79c94
Wenlong(Stan) Wang <stan.wenlong.wang@gmail.com>
2022-01-12 11:19:16 +1300

    This is a commit on a crazy function test of a different branch.

--- my_program.py
+++ my_program.py
@@ -1,5 +1,9 @@
 def my_function():
 	print('This is a main program function.')
 
+def my_crazy_function_test():
+	print('I have tested git restore. This is a crazy function test I want to commit.')
+
 if __name__ == "__main__":
 	my_function()
+	my_crazy_function_test()
